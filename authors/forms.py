df015232da3a90bcc21bbed16d9d7a8bf8b28ab7
davilxn <davi.santos.alexandrino@gmail.com>
2023-02-05 19:31:09 -0300

    Reorganizando os arquivos da aplicação

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class RegisterForm(forms.ModelForm):
+    password2 = forms.CharField(
+        required = True,
+        widget = forms.PasswordInput(attrs={
+            'placeholder': 'Digite novamente sua senha'
+        }),
+        error_messages={
+            'required': 'Este campo deve ser preenchido'
+        },
+        label="Repita sua senha:"
+    )
+
+    """password = forms.CharField(
+        required=True,
+        widget= forms.PasswordInput(attrs={
+            'placeholder': 'Digite sua senha'
+        }),
+        error_messages={
+            'required': 'Este campo deve ser preenchido',
+            'invalid': 'Erro'
+        },
+        label='Senha:'
+    )"""
     class Meta:
         model = User           
         fields = [
@@ -10,4 +33,81 @@ class RegisterForm(forms.ModelForm):
             'last_name',
             'email',
             'password',
-          ]
+        ]
+
+        labels = {
+            'username': 'Nome de usuário:',
+            'first_name': 'Nome:',
+            'last_name': 'Sobrenome:',
+            'email': 'E-mail:',
+            'password': 'Senha:'
+        }
+
+        help_texts = {
+            'username': '150 caracteres ou menos. Letras, números e @/.+-_',
+            'email': 'Este email deve ser um email válido.',
+            'password': 'Por segurança, crie uma senha formada por letras e números'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'Este campo deve ser preenchido.'
+            },
+            'password': {
+                'required':'Este campo deve ser preenchido.'    
+            },
+            'first_name': {
+                'invalid': 'Não é permitido'
+            }
+        }
+
+        widgets = {
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Digite sua senha'
+            }),
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Digite seu nome'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Digite seu sobrenome'
+            }),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'Digite seu email'
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Digite seu nome de usuário'
+            }),
+        }
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+        if 'John Doe' in data or 'Jane Doe' in data:
+            raise ValidationError('Não são permitidos desconhecidos.', code='invalid')
+        return data
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError('Este e-mail já foi cadastrado.')
+
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username', '')
+        exists = User.objects.filter(username=username).exists()
+        if exists:
+            raise ValidationError('Um usuário com este nome já existe.')
+
+        return username
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+            'password':'As senhas estão diferentes',
+            'password2': 'As senhas estão diferentes'
+            })
--- authors/views.py
+++ authors/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', context={
         'form': form,
     })
@@ -12,7 +14,17 @@ def register_view(request):
 def register_create(request):
     if not request.POST:
         raise Http404()
-    form = RegisterForm(request.POST)
-    return render(request, 'authors/pages/register_view.html', context={
-        'form': form,
-    })
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Seu usuário foi criado com sucesso. Por favor, efetuar log in.')
+        del(request.session['register_form_data'])
+
+    return redirect('authors-register')
+    
